3c87882ab5d6fe612b7795e096245669ca177a56
Asami Tachibana <atachibana2612@gmail.com>
2018-09-28 20:50:36 -0400

    Another revision, now to start commenting!

--- majority_element.swift
+++ majority_element.swift
@@ -12,6 +12,7 @@ import Foundation
 
 var exampleArray_1 = [Int](arrayLiteral: 3,2,3)
 var exampleArray_2 = [Int](arrayLiteral: 2,2,1,1,1,2,2)
+var exampleArray_3 = [Int](arrayLiteral: 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,7)
 
 func majorityElement(_ nums: [Int]) -> Int {
   let EA_CLONE = nums
@@ -20,21 +21,25 @@ func majorityElement(_ nums: [Int]) -> Int {
   var i = 0
   var k = 0
   while i < uniqueVal.count {
-    print("i=",i)
     while k < EA_CLONE.count {
       if (uniqueVal[i] == EA_CLONE[k]) {
         countingIndex[i] = countingIndex[i] + 1
       }
-      print("k=",k)
       k = k+1
     }
     i = i+1
     k=0
   }
+  let maxIndex = countingIndex.max()!
+  let maxIndex_loc = countingIndex.firstIndex(of: maxIndex)
+  let commonValue = uniqueVal[maxIndex_loc!]
   print(EA_CLONE)
-  print(uniqueVal)
-  print(countingIndex)
-  return 0
+  print("Largest integer value is:",commonValue)
+  print("Largest integer appears in array:", maxIndex, "times")
+  return commonValue
 }
 
+/* Execute all these functions */
 majorityElement(exampleArray_1)
+majorityElement(exampleArray_2)
+majorityElement(exampleArray_3)
